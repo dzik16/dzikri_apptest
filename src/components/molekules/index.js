6fc1c8ea3716e82ebe67b168bcac56492d88d9ed
M. Dzikri Alfarisyi <dikrialfarisi9@gmail.com>
2023-07-09 05:20:01 +0700

    fixing home

--- src/components/molekules/index.js
+++ src/components/molekules/index.js
@@ -1,11 +1,7 @@
 import CardFoto from "./CardFoto";
 import EmptyState from "./EmptyState";
-import BottomUpload from "./BottomSheetUpload";
-import Upload from "./UploadFoto";
 
 export {
   CardFoto,
   EmptyState,
-  BottomUpload,
-  Upload,
 };
